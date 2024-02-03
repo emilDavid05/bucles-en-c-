5f944d2566825bfb1dd1e02f060ec9c8fe2a162f
Laptop <Laptop@DESKTOP-AAIMPLJ>
2024-02-03 17:31:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine();
+
+
+
+int numero;
+
+Console.Write("Introduzca un numero entero positivo: ");
+
+numero = int.Parse(Console.ReadLine());
+
+while (numero > 0)
+
+{
+    Console.Write("Introduzca un numero entero positivo: ");
+
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("ha introducido un numero entero negativo. Fijate bien mamaguevo.");
